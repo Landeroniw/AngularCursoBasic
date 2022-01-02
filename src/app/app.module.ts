cad6c07d5c8836d56932dd455ed4c9169cd5d0c3
David Landeros <49451920+Landeroniw@users.noreply.github.com>
2022-01-02 17:27:25 -0600

    first update
    Subiendo el Proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { Contador } from './contador/contador.component';
+import { HeroesModule } from './heroes/heroes.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Contador
+  ],
+  imports: [
+    BrowserModule,
+    HeroesModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
